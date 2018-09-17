d6b4edbf9b91796e69327c1b7c9f9f5bacd23e6f
Sergio Hidalgo <sergi98hf-@hotmail.com>
2018-09-17 07:56:15 -0600

    movile styles for the webpage

--- I do magic/promises-callbacks-js/gulpfile.js
+++ I do magic/promises-callbacks-js/gulpfile.js
@@ -7,7 +7,9 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var cssnano = require('gulp-cssnano');
 gulp.task('sass', function () {
-	gulp.src(['./src/css/main.scss'])
+	gulp.src(['./src/css/main.scss', './src/css/header.scss', 
+	'./src/css/body-top.scss', './src/css/body.scss', 
+	'./src/css/footer.scss'])
 		.pipe(plumber())
 		.pipe(sass())
 		.pipe(cssnano())
